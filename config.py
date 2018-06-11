406084e20ff05b2b2fc8b4fa830ccb1085807c7d
ChenGrapes <735119890@qq.com>
2018-06-11 23:45:24 +0800

    配置文件config的抽取

---
+++ config.py
@@ -0,0 +1,39 @@
+import redis
+
+class Config(object):
+
+    DEBUG = True
+    SECRET_KEY = "fdhgsbjfsa"
+
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #创建redis
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # 配置session
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(REDIS_HOST,REDIS_PORT) # 存储在redis中
+    SESSION_USE_SIGNER = True # 设置签名
+    PERMANENT_SESSION_LIFETIME = 3600 * 24 * 2  # 设置session有效时间
+
+# 开发模式
+class DeveloperConfig(Config):
+    pass
+# 生产模式
+class ProductConfig(Config):
+    DEBUG = False
+    pass
+# 测试模式
+class TestingConfig(Config):
+    pass
+
+# 设置统一访问入口
+config_dict ={
+    "develop":DeveloperConfig,
+    "product":ProductConfig,
+    "testing":TestingConfig
+
+}
