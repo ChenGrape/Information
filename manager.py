e39ad400bb1e0d9e909a6e385064533f9c0824d3
ChenGrapes <735119890@qq.com>
2018-06-11 23:02:53 +0800

    创建session

--- manager.py
+++ manager.py
@@ -1,10 +1,13 @@
 import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__)
 
 class Config(object):
+
+    SECRET_KEY = "fdhgsbjfsa"
 
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/information"
@@ -14,6 +17,12 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    # 配置session
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(REDIS_HOST,REDIS_PORT) # 存储在redis中
+    SESSION_USE_SIGNER = True # 设置签名
+    PERMANENT_SESSION_LIFETIME = 3600 * 24 * 2  # 设置session有效时间
+
 app.config.from_object(Config)
 
 #创建数据库连接
@@ -21,6 +30,9 @@ db = SQLAlchemy(app)
 
 # 创建redis
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT,decode_responses=True)
+
+# 创建session，添加到app中
+Session(app)
 
 @app.route("/")
 def hello_world():
