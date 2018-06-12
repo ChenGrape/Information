875e33635dcd1d4fd2f62022cec180a833b90c9f
ChenGrape <735119890@qq.com>
2018-06-12 13:28:43 +0800

    首页视图index.html访问函数封装

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,17 +1,10 @@
 from info import redis_store
 from . import index_blu
+from flask import render_template
+
 
 @index_blu.route("/", methods=['GET', 'POST'])
 def hello_world():
-    redis_store.set("name", "zhangsan")
-    name = redis_store.get("name")
-    print(name)
-    # logging.debug('debugAAAA')
-    # logging.info('infoAAA')
-    # logging.error('errorAAA')
-    #
-    # current_app.logger.debug('debugBBB')
-    # current_app.logger.info('infoBBB')
-    # current_app.logger.error('errorBBB')
 
-    return "hello word!"
+
+    return render_template("news/index.html")
