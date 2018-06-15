8085c70880ea9b7ac3e662652b5fe3b738d9ddec
ChenGrape <735119890@qq.com>
2018-06-15 11:29:03 +0800

    新闻信息显示前后端实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,70 @@
-from flask import session
+from flask import request
+from flask import session, jsonify
 
 from info import constants
 from info import redis_store
 from info.models import User, News, Category
+from info.utils.response_code import RET
 from . import index_blu
 from flask import render_template,current_app
 
+
+#首页新闻列表
+# 请求路径: /newslist
+# 请求方式: GET
+# 请求参数: cid,page,per_page
+# 返回值: data数据
+@index_blu.route('/newslist')
+def news_list():
+    """
+    思路分析
+    1.获取参数
+    2.校验参数,转换参数类型
+    3.根据条件查询数据
+    4.将查询到的分类对象数据,转成字典
+    5.返回响应请求
+    :return:
+    """
+    # 1.获取参数
+    cid =  request.args.get("cid")
+    page =  request.args.get("page",1)
+    per_page =  request.args.get("per_page",10)
+
+    # 2.校验参数,转换参数类型
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+        per_page = 10
+
+    # 3.根据条件查询数据
+    try:
+        #判断分类编号是否不等于 1
+        filters = []
+        if cid != "1":
+            filters.append(News.category_id == cid)
+
+        #查询新闻数据根据条件
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,per_page,False)
+
+        #获取分页中的内容,总页数,当前页,当前页的所有对象
+        totalPage = paginate.pages
+        currentPage = paginate.page
+        items = paginate.items
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据查询失败")
+
+    # 4.将查询到的分类对象数据,转成字典
+    newsList = []
+    for news in items:
+        newsList.append(news.to_dict())
+
+    # 5.返回响应请求
+    return jsonify(errno=RET.OK,errmsg="获取数据成功",cid=cid,currentPage=currentPage,totalPage=totalPage,newsList=newsList)
 
 @index_blu.route("/", methods=['GET', 'POST'])
 def hello_world():
