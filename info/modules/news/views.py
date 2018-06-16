e3ef5c904091de2269ccbfebfaa7e983f0488826
ChenGrape <735119890@qq.com>
2018-06-16 10:53:12 +0800

    页面基类抽取，base.html

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -4,4 +4,4 @@ from flask import render_template
 @news_blu.route("/<int:news_id>")
 def new_detail(news_id):
 
-    return render_template("news/detail.html")
+    return render_template("news/detail.html", data ={} )
